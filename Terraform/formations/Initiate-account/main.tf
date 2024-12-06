4339f3a3872bece8b8c730ddadd891aac1a14d7d
Brigthain kargong <kbrigthain@gmail.com>
2024-12-05 23:00:05 -0500

    Updated subnets

--- Terraform/formations/Initiate-account/main.tf
+++ Terraform/formations/Initiate-account/main.tf
@@ -8,3 +8,14 @@ module "vpc" {
   common = var.common
   vpc    = var.vpc
 }
+
+#--------------------------------------------------------------------
+# Subnets - Creates private and public subnets
+#--------------------------------------------------------------------
+module "subnets" {
+  source = "git@github.com:njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnets?ref=v1.3"
+  # for_each = var.vpc != null ? { for item in var.vpc : item.key => item } : {} 
+
+  common = var.common
+  vpc    = var.vpc
+}
