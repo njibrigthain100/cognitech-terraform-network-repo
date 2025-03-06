cf8cccafd7fda942bce62d42529747ae2acb87c2
Brigthain kargong <kbrigthain@gmail.com>
2025-03-06 00:11:52 -0500

    updated url

--- Terraform/formations/Simple-network/main.tf
+++ Terraform/formations/Simple-network/main.tf
@@ -2,7 +2,7 @@
 # VPC - Creates a VPC  to the target account
 #--------------------------------------------------------------------
 module "vpc" {
-  source = "git@github.com:njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/vpc?ref=v1.35"
+  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/vpc?ref=v1.35"
   vpc    = var.vpc
   common = var.common
 }
@@ -12,7 +12,7 @@ module "vpc" {
 #--------------------------------------------------------------------
 
 module "public_subnets" {
-  source         = "git@github.com:njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnets/public_subnets?ref=v1.35"
+  source         = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/subnets/public_subnets?ref=v1.35"
   vpc_id         = module.vpc.vpc_id
   public_subnets = var.vpc.public_subnets
   common         = var.common
@@ -20,7 +20,7 @@ module "public_subnets" {
 
 
 module "public_route" {
-  source = "git@github.com:njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Routes/public_routes?ref=v1.35"
+  source = "git::https://github.com/njibrigthain100/Cognitech-terraform-iac-modules.git//terraform/modules/Routes/public_routes?ref=v1.35"
   vpc_id = module.vpc.vpc_id
   public_routes = {
     public_gateway_id      = module.vpc.igw_id
